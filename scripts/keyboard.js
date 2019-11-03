a407b7e96e9f80b9ca3a161e54642572a00b2356
Max Garanin <maxgaranin@gmail.com>
2019-11-03 11:56:52 +0400

    feat: add enter, backspace, del support

--- scripts/keyboard.js
+++ scripts/keyboard.js
@@ -1,14 +1,10 @@
 const textInput = document.querySelector('.text-input');
 const keyboard = document.querySelector('.keyboard');
 
-document.addEventListener('keydown', (event) => {
-    event.preventDefault();
-    const keyDiv = document.getElementById(event.key);
-    keyDiv.click();
+document.addEventListener('keydown', () => {
 });
 
 document.addEventListener('keyup', () => {
-    
 });
 
 document.querySelectorAll('.keyboard__key').forEach(keyDiv => {
@@ -24,8 +20,36 @@ document.querySelectorAll('.keyboard__key').forEach(keyDiv => {
 
     keyDiv.addEventListener('click', (event) => {
         const elem = event.target;
-        if (elem.classList.contains('keyboard__key-service')) return;
-
-        textInput.textContent += elem.textContent;
+        if (elem.classList.contains('keyboard__key-service')) {
+            return;
+        }
+        else if (elem.classList.contains('keyboard__key-enter')) {
+            let start = textInput.selectionStart;
+            textInput.textContent =
+                textInput.textContent.slice(0, start - 1) + '\r\n' +
+                textInput.textContent.slice(start);
+            textInput.selectionStart = start;
+        }
+        else if (elem.classList.contains('keyboard__key-backspace')) {
+            let start = textInput.selectionStart;
+            if (start > 0) {
+                textInput.textContent =
+                    textInput.textContent.slice(0, start - 1) +
+                    textInput.textContent.slice(start);
+                textInput.selectionStart = start - 1;
+            }
+        }
+        else if (elem.classList.contains('keyboard__key-del')) {
+            let start = textInput.selectionStart;
+            if (start < textInput.textContent.length) {
+                textInput.textContent =
+                    textInput.textContent.slice(0, start) +
+                    textInput.textContent.slice(start + 1);
+                textInput.selectionStart = start;
+            }
+        }
+        else {
+            textInput.textContent += elem.textContent;
+        }
     });
 });
