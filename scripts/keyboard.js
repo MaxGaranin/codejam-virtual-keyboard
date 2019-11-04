94fd1f1da2f2cbbf06878106098608340377c472
Max Garanin <maxgaranin@gmail.com>
2019-11-05 00:03:34 +0400

    feat: add some features (shift, capslock capitalization, change language)

--- scripts/keyboard.js
+++ scripts/keyboard.js
@@ -1,24 +1,57 @@
 const textInput = document.querySelector('.text-input');
 const keyboard = document.querySelector('.keyboard');
+const keyButtons = document.querySelectorAll('.keyboard__key');
 
-document.addEventListener('keydown', () => {
+let currentLanguage = "Ru";
+let isShiftPressed = false;
+
+document.addEventListener('keydown', (event) => {
+    if (event.code === "ShiftRight") {
+        if (currentLanguage === "En") {
+            currentLanguage = "Ru";
+        }
+        else {
+            currentLanguage = "En";
+        }
+        changeLanguage();
+    }
 });
+
+function changeLanguage() {
+    document.querySelectorAll('.keyboard__key').forEach(keyBtn => {
+        const lg = LANGUAGES[currentLanguage];
+        const key = lg[keyBtn.id];
+        if (key) {
+            keyBtn.innerHTML = key;
+        }
+    });
+}
 
 document.addEventListener('keyup', () => {
 });
 
-document.querySelectorAll('.keyboard__key').forEach(keyDiv => {
-    keyDiv.addEventListener('mousedown', (event) => {
+document.querySelectorAll('.keyboard__key').forEach(keyBtn => {
+    changeLanguage();
+
+    keyBtn.addEventListener('mousedown', (event) => {
         const elem = event.target;
         elem.style.background = 'green';
+        if (elem.classList.contains('keyboard__key-shift')) {
+            keyButtons.forEach(keyButton => keyButton.classList.add('keyboard__key_uppercase'));
+            isShiftPressed = true;
+        }
     });
 
-    keyDiv.addEventListener('mouseup', (event) => {
+    keyBtn.addEventListener('mouseup', (event) => {
         const elem = event.target;
         elem.style.background = '';
+        if (elem.classList.contains('keyboard__key-shift') && isShiftPressed) {
+            keyButtons.forEach(keyButton => keyButton.classList.remove('keyboard__key_uppercase'));
+            isShiftPressed = false;
+        }
     });
 
-    keyDiv.addEventListener('click', (event) => {
+    keyBtn.addEventListener('click', (event) => {
         const elem = event.target;
         if (elem.classList.contains('keyboard__key-service')) {
             return;
@@ -48,8 +81,30 @@ document.querySelectorAll('.keyboard__key').forEach(keyDiv => {
                 textInput.selectionStart = start;
             }
         }
+        else if (elem.classList.contains('keyboard__key-capslock')) {
+            keyButtons.forEach(keyButton => {
+                keyButton.classList.toggle('keyboard__key_uppercase');
+            });
+            elem.classList.toggle('keyboard__key-capslock-on');            
+        }
+        else if (elem.classList.contains('material-icons')) {
+            if (elem.parentElement.classList.contains('keyboard__key-arrow-up')) {
+                textInput.textContent += '▲';
+            }
+            else if (elem.parentElement.classList.contains('keyboard__key-arrow-down')) {
+                textInput.textContent += '▼';
+            }            
+            else if (elem.parentElement.classList.contains('keyboard__key-arrow-left')) {
+                textInput.textContent += '◀';
+            }            
+            else if (elem.parentElement.classList.contains('keyboard__key-arrow-right')) {
+                textInput.textContent += '▶';
+            }            
+        }
         else {
             textInput.textContent += elem.textContent;
         }
+
+        event.stopPropagation();
     });
 });
